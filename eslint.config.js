2cd4c27401332d26c1c659293867bc96da149e28
Kustiche <ChernichnyKustik@mail.ru>
2026-09-23 17:34:55 +0300

    blin

--- eslint.config.js
+++ eslint.config.js
@@ -5,32 +5,32 @@ import prettierConfig from 'eslint-config-prettier';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-	{
-		ignores: ['**/*.js', '!assets/**/*.js'],
-	},
-	{
-		plugins: {
-			prettier: prettierPlugin,
-		},
-	},
-	{
-		languageOptions: {
-			globals: {
-				...globals.browser,
-				...globals.es2020,
-			},
-		},
-	},
-	pluginJs.configs.recommended,
-	{
-		rules: {
-			...prettierConfig.rules,
-			'no-console': 'warn',
-			'no-alert': 'warn',
-			'no-debugger': 'warn',
-			'no-var': 'error',
-			'prefer-const': 'error',
-			'prefer-arrow-callback': 'error',
-		},
-	},
+  {
+    ignores: ['**/*.js', '!assets/**/*.js'],
+  },
+  {
+    plugins: {
+      prettier: prettierPlugin,
+    },
+  },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.es2020,
+      },
+    },
+  },
+  pluginJs.configs.recommended,
+  {
+    rules: {
+      ...prettierConfig.rules,
+      'no-console': 'warn',
+      'no-alert': 'warn',
+      'no-debugger': 'warn',
+      'no-var': 'error',
+      'prefer-const': 'error',
+      'prefer-arrow-callback': 'error',
+    },
+  },
 ];
